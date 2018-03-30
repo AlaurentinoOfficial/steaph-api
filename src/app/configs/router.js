2622d583ea20a1972bca48c8b449a002d3d1ecc9
Anderson Laurentino <alaurentino.br@gmail.com>
2018-03-29 23:15:23 -0300

    Add Router config

---
+++ src/app/configs/router.js
@@ -0,0 +1,5 @@
+import * as jwt from "jsonwebtoken";
+import { Server } from "../../server";
+
+exports.Router = (app) => {
+}
--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 
-import {DbConfig} from './app/config/database'
+import { DbConfig } from './app/config/database'
+import { Router } from './app/configs/router';
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
@@ -11,6 +12,8 @@ app.set('crypt_key', 'dfhads8g3bfosdfs')
 app.set('port', process.env.PORT || 8080)
 
 var argv = process.argv.slice(2)
+
+Router(app)
 DbConfig(argv.indexOf("--docker") >= 0 ? "mongodb://mongo/CRM" : "mongodb://localhost:27017/CRM")
 
 exports.Server = app
