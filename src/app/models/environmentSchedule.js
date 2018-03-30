87e57315e030df37ee3185a3fad49e7bb8f0b867
Anderson Laurentino <alaurentino.br@gmail.com>
2018-03-29 23:29:55 -0300

    Add Environment Schedule schema

---
+++ src/app/models/environmentSchedule.js
@@ -0,0 +1,11 @@
+import * as mongoose from "mongoose";
+import * as relationship from "mongoose-relationship";
+
+let environmentScheduleSchema = new mongoose.Schema({
+    environment: {type: mongoose.Schema.ObjectId, ref:"Environment", childPath:"schedule"},
+    status: {type: Boolean, required: true},
+    start: {type: Date, required: true},
+    end: {type: Date, required: true}
+});
+environmentScheduleSchema.plugin(relationship, { relationshipPathName:'environment' });
+export const EnvironmentScheduleSchema = mongoose.model('EnvironmentSchedule', environmentScheduleSchema);
