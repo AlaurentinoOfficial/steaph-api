824b1ee4d340778e18378810051b8e311d722c64
Anderson Laurentino <alaurentino.br@gmail.com>
2018-03-29 23:28:25 -0300

    Add Environment Status schema

---
+++ src/app/models/environmentStatus.js
@@ -0,0 +1,13 @@
+import * as mongoose from "mongoose";
+import * as relationship from "mongoose-relationship";
+
+let environmentStatusSchema = new mongoose.Schema({
+    environment: {type: mongoose.Schema.ObjectId, ref:"Environment", childPath:"status"},
+    status: {type: Boolean, required: true},
+    motion: {type: Boolean, required: true},
+    temperature: {type: Number, required: true},
+    noisy: {type: Number, required: true},
+    gas: {type: Number, required: true}
+});
+environmentStatusSchema.plugin(relationship, { relationshipPathName:'environment' });
+export const EnvironmentStatusSchema = mongoose.model('EnvironmentStatus', environmentStatusSchema);
