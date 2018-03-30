07d085ed900ad7c5f3105047858147920bb8d640
Anderson Laurentino <alaurentino.br@gmail.com>
2018-03-29 23:03:00 -0300

    Add Strings report

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.set('crypt_key', 'dfhads8g3bfosdfs')
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 8080)
 
 var argv = process.argv.slice(2)
 DbConfig(argv.indexOf("--docker") >= 0 ? "mongodb://mongo/CRM" : "mongodb://localhost:27017/CRM")
