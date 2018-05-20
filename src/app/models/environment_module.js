45a9d264e0de43926254fa6d4a1592164d006100
Anderson Laurentino <alaurentino.br@gmail.com>
2018-05-20 12:43:41 -0300

    Update the Environment Schedule

--- src/app/models/environment_module.js
+++ src/app/models/environment_module.js
@@ -4,10 +4,7 @@ var relationship = require("mongoose-relationship")
 let environmentModuleSchema = new mongoose.Schema({
     environment: {type: mongoose.Schema.Types.ObjectId, ref:"Environment", childPath:"environments", required: true},
     name: {type: String, required: true},
-    address: {type: String, required: true, min: 25, max: 25, unique: true},
-    status: {type: Boolean, default: false, required: true},
-    plotstatus: [{type: mongoose.Schema.ObjectId, ref:"EnvironmentStatus", required: false}],
-    schedule: [{type: mongoose.Schema.Types.ObjectId, ref:"EnvironmentSchdule", required: false}]
+    address: {type: String, required: true, unique: true}
 });
 
 environmentModuleSchema.plugin(relationship, { relationshipPathName:'solution' });
