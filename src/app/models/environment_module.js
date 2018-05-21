57d97726f945990a8438be82d92e284e0cd3a161
Anderson Laurentino <alaurentino.br@gmail.com>
2018-05-20 23:17:00 -0300

    Remove all the Environment Module relationship with Environment

--- src/app/models/environment_module.js
+++ src/app/models/environment_module.js
@@ -2,10 +2,10 @@ var mongoose = require("mongoose")
 var relationship = require("mongoose-relationship")
 
 let environmentModuleSchema = new mongoose.Schema({
-    environment: {type: mongoose.Schema.Types.ObjectId, ref:"Environment", childPath:"modules", required: false},
+    // environment: {type: mongoose.Schema.Types.ObjectId, ref:"Environment", childPath:"modules", required: false},
     name: {type: String, required: true},
     address: {type: String, required: true, unique: true}
 });
 
-environmentModuleSchema.plugin(relationship, { relationshipPathName:'solution' });
+// environmentModuleSchema.plugin(relationship, { relationshipPathName:'solution' });
 exports.EnvironmentModuleSchema = mongoose.model('EnvironmentModule', environmentModuleSchema);
