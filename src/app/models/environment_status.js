2580469c753240d9173265a31c92fb49b6f3b5ed
Anderson Laurentino <alaurentino.br@gmail.com>
2018-09-16 18:10:03 -0300

    Remove the Models schema

--- src/app/models/environment_status.js
+++ src/app/models/environment_status.js
@@ -6,10 +6,7 @@ let environmentStatusSchema = new mongoose.Schema({
     date: {type: Date, required: true},
     status: {type: Boolean, required: true},
     power: {type: Number, required: false},
-    voltage: {type: Number, required: false},
-    temperature: {type: Number, required: false},
-    external_temperature: {type: Number, required: false},
-    luminosity: {type: Number, required: false}
+    voltage: {type: Number, required: false}
 });
 
 environmentStatusSchema.plugin(relationship, { relationshipPathName:'environment' });
