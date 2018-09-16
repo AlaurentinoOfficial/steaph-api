07cb310c547a8f57fe6aef46e4c1f88f923398df
Anderson Laurentino <alaurentino.br@gmail.com>
2018-09-16 18:21:53 -0300

    Add date_creation attr in Solution schema

--- src/app/models/solution.js
+++ src/app/models/solution.js
@@ -3,10 +3,19 @@ var relationship = require("mongoose-relationship")
 
 let solutionSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    status: {type: Boolean, default: true, require: false},
     cpf_cnpj: {type: String, required: true},
+    status: {type: Boolean, default: true, require: false},
+    date_creation: {type: Date, required: false},
     type: {type: String, enum: ["physical", "legal"], required: true},
     users: [{type: mongoose.Schema.Types.ObjectId, ref:"User", required: false}],
     environments: [{type: mongoose.Schema.Types.ObjectId, ref:"Environment", required: false}]
 })
+
+solutionSchema.pre('save', (next) => {
+    if(this.isNew)
+        this.date_creation = new Date()
+
+    next()
+})
+
 exports.SolutionSchema = mongoose.model('Solution', solutionSchema)
